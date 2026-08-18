2c8a5561953dcba4ef68e9a784026792f0689170
DelaCruz-Riceerich <riceedc292006@gmail.com>
2026-08-18 09:23:13 +0800

    Remove add method from linked list
    Removed the add method from the linked list implementation.

--- linked.js
+++ linked.js
@@ -11,18 +11,7 @@ class LinkedList {
         this.head = null;
     }
 
-    add(data) {
-        const newNode = new ListNode(data);
-        if (this.head === null) {
-            this.head = newNode;
-            return;
-        }
-        let current = this.head;
-        while (current.next !== null) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
+    
 
     display() {
         let current = this.head;

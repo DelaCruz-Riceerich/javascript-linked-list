07825b1b8bc2fb02bad5356ee0f5805bb664ecc2
DelaCruz-Riceerich <riceedc292006@gmail.com>
2026-08-18 09:24:52 +0800

    Add method to append data to linked list

--- linked.js
+++ linked.js
@@ -11,7 +11,18 @@ class LinkedList {
         this.head = null;
     }
 
-    
+    add(data) {
+        const newNode = new ListNode(data);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
 
     display() {
         let current = this.head;
